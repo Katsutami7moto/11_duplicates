7e2e2c5f99d63a506712b3f5b286f89087ea880b
napan <alyandaj87>
2017-07-01 11:14:02 +0300

    Added imports, classes and function names

--- duplicates.py
+++ duplicates.py
@@ -1,3 +1,17 @@
+import os
+from os.path import join, getsize, exists
+from collections import namedtuple
+
+File = namedtuple('File', 'name size dir_path')
+Duplicate = namedtuple('Duplicate', 'name size all_paths')
+
+
+def find_duplicates(source_path: str) -> list:
+    pass
+
+
+def print_duplicates(duplicates: list):
+    pass
 
 if __name__ == '__main__':
     pass
